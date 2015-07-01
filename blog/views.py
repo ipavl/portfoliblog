ef79c99fc159059e1a2caae0431ec34a33cf0aef
ipavl <ipavl@users.noreply.github.com>
2015-06-30 22:19:48 -0400

    Add single post view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404
 from blog.models import Post, Category
 
 def index(request):
     posts = Post.objects.order_by('-date')
     return render(request, 'blog/index.html', {"posts": posts})
+
+def view_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render_to_response('blog/post.html', {"post": post})
